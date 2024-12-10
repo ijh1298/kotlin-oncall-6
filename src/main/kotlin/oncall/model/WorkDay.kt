34688e063b189622fe61341e066c1cf19bd0e906
ijh1298 <imjh0417@gmail.com>
2024-12-10 18:26:45 +0900

    chore: WorkDay toString 메소드 오버라이드

--- src/main/kotlin/oncall/model/WorkDay.kt
+++ src/main/kotlin/oncall/model/WorkDay.kt
@@ -7,4 +7,9 @@ data class WorkDay(
     val worker: String,
     val isHoliday: Boolean
 ) {
+    override fun toString(): String {
+        if (isHoliday)
+            return "${month}월 ${date}일 $day(휴일) $worker"
+        return "${month}월 ${date}일 $day $worker"
+    }
 }
