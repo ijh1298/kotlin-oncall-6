653e8d0537ceccdb95f3ccd068ca3403750f55c1
ijh1298 <imjh0417@gmail.com>
2024-12-10 18:53:49 +0900

    chore: 컨트롤러 내 반복 입력받는 로직 수정

--- src/main/kotlin/oncall/controller/OncallController.kt
+++ src/main/kotlin/oncall/controller/OncallController.kt
@@ -1,27 +1,35 @@
 package oncall.controller
 
+import oncall.model.Day
 import oncall.model.WorkDay
+import oncall.service.OncallService
 import oncall.validator.MonthDayValidator
 import oncall.validator.WorkerValidator
 import oncall.view.InputView
+import oncall.view.OutputView
 
 class OncallController(
     private val inputView: InputView = InputView(),
+    private val outputView: OutputView = OutputView()
 ) {
     private var month: Int = 1
     private var day = "월"
-    private lateinit var weekdayOrder: List<String>
-    private lateinit var holidayOrder: List<String>
+    private val weekdayOrder = ArrayDeque<String>()
+    private val holidayOrder = ArrayDeque<String>()
+    private lateinit var monthlySchedule: List<WorkDay>
 
     fun run() {
         setMonthDay()
         setWeekdayOrder()
         setHolidayOrder()
+        monthlySchedule = OncallService.getMonthlySchedule(Day.valueOf(day), month, weekdayOrder, holidayOrder)
+        outputView.printResult(monthlySchedule)
     }
 
     private fun setMonthDay() {
-        val rawMonthDay = inputView.inputMonthDay()
+        var rawMonthDay = ""
         loopUntilValid {
+            rawMonthDay = inputView.inputMonthDay()
             MonthDayValidator(rawMonthDay).validate()
         }
         val parsed = rawMonthDay.split(',').map { it }
@@ -30,21 +38,23 @@ class OncallController(
     }
 
     private fun setWeekdayOrder() {
-        val rawWorkers = inputView.inputWeekdays()
+        var rawWorkers = ""
         loopUntilValid {
+            rawWorkers = inputView.inputWeekdays()
             WorkerValidator(rawWorkers).validateWeekdayWorkers()
         }
         val parsed = rawWorkers.split(',').map { it }
-        weekdayOrder = parsed
+        parsed.forEach { weekdayOrder += it }
     }
 
     private fun setHolidayOrder() {
-        val rawWorkers = inputView.inputHolidays()
+        var rawWorkers = ""
         loopUntilValid {
+            rawWorkers = inputView.inputHolidays()
             WorkerValidator(rawWorkers).validateHolidayWorkers(weekdayOrder)
         }
         val parsed = rawWorkers.split(',').map { it }
-        holidayOrder = parsed
+        parsed.forEach { holidayOrder += it }
     }
 
     private fun <T> loopUntilValid(action: () -> T): T {
