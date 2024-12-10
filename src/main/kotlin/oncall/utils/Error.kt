21b656d87b14afd3cdce35432d23225d66026fc2
ijh1298 <imjh0417@gmail.com>
2024-12-10 17:17:26 +0900

    feat: WorkerValidator 추가

--- src/main/kotlin/oncall/utils/Error.kt
+++ src/main/kotlin/oncall/utils/Error.kt
@@ -4,4 +4,7 @@ enum class Error(
     val msg: String
 ) {
     INVALID_MONTH_DAY("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요."),
+
+    INVALID_WORKER("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요."),
+    NOT_CONTAINED_ALL_WORKER("[ERROR] 평일 근무자가 모두 휴일 근무자에 포함되어야 합니다. 다시 입력해 주세요.")
 }
---
+++ src/main/kotlin/oncall/validator/WorkerValidator.kt
@@ -0,0 +1,33 @@
+package oncall.validator
+
+import oncall.utils.Error
+
+class WorkerValidator(
+    private val rawWorkers: String
+) {
+    private lateinit var workers: List<String>
+
+    fun validateWeekdayWorkers(): Boolean {
+        validateWorkers()
+        return true
+    }
+
+    fun validateHolidayWorkers(weekdayWorkers: List<String>): Boolean {
+        validateWeekdayWorkers()
+        require(isAllInWeekdayWorkers(weekdayWorkers)) { Error.NOT_CONTAINED_ALL_WORKER }
+        return true
+    }
+
+    private fun isAllInWeekdayWorkers(weekdayWorkers: List<String>): Boolean {
+        val holidayWorkers = rawWorkers.split(',').map { it }
+        return holidayWorkers.containsAll(weekdayWorkers)
+    }
+
+    private fun validateWorkers() {
+        val parsed = rawWorkers.split(',').map { it }
+
+        require(parsed.all { it.length in 1..5 }) { Error.INVALID_WORKER.msg }
+        require(parsed.size in 5..35) { Error.INVALID_WORKER.msg }
+        require(parsed.toSet().size == parsed.size) { Error.INVALID_WORKER.msg }
+    }
+}
